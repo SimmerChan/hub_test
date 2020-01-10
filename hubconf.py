99fdee1177ba2a346699e1d91e34765be3dc191e
SimmerChan <7698590@qq.com>
2020-01-10 17:55:30 +0800

    hugconf changed

--- hubconf.py
+++ hubconf.py
@@ -7,6 +7,6 @@ dependencies = ["torch", "torchvision"]
 
 
 def model():
-    s2s_ckpoint = torch.hub.load_state_dict_from_url('checkpoint', progress=False)
-    cnn_ckpoint = torch.hub.load_state_dict_from_url('checkpoint', progress=False)
+    s2s_ckpoint = torch.hub.load_state_dict_from_url('https://github.com/SimmerChan/hub_test/releases/download/v1.0/best_img2poem_transformer_wo_kws_1.pt', progress=True)
+    cnn_ckpoint = torch.hub.load_state_dict_from_url('https://github.com/SimmerChan/hub_test/releases/download/v1.0/best_multi_label_encoder_weight_16.pt', progress=True)
     return Image2PoemGenerator(s2s_ckpoint, cnn_ckpoint)
