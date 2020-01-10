d9c20e8765e3de5c11e90c21d104ebde4a839963
SimmerChan <7698590@qq.com>
2020-01-10 18:54:45 +0800

    checkpoint bytes

--- hubconf.py
+++ hubconf.py
@@ -10,6 +10,6 @@ dependencies = ["torch", "torchvision"]
 def model():
     s2s_ckpoint = torch.hub.load_state_dict_from_url('https://github.com/SimmerChan/hub_test/releases/download/v1.0/best_img2poem_transformer_wo_kws_1.pt', progress=True)
     cnn_ckpoint = torch.hub.load_state_dict_from_url('https://github.com/SimmerChan/hub_test/releases/download/v1.0/best_multi_label_encoder_weight_16.pt', progress=True)
-    print(type(s2s_ckpoint))
-    print(type(cnn_ckpoint))
+    print(s2s_ckpoint.keys())
+    print(cnn_ckpoint.keys())
     return Image2PoemGenerator(BytesIO(s2s_ckpoint), BytesIO(cnn_ckpoint))
